e4a135780b4c94944e8d0bb1cb3467099835014b
Sabrina <sabrinajackson@nevada.unr.edu>
2024-04-13 15:04:04 -0700

    added silly effects

--- Assets/Scripts/Enemies/Attack.cs
+++ Assets/Scripts/Enemies/Attack.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public int killCount = 0;
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Enemy")
         {
             Debug.Log("I RAN SOMEONE OVER");
             Destroy(collider.gameObject);
+            killCount++;
         }
     }
 }
